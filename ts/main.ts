d03103bbd58404f7bbb4f616c474d46a5c7b86b5
reebelbeebel <90023189+reebelbeebel@users.noreply.github.com>
2021-12-06 10:25:55 +0300

    uploaded
    Uploaded

---
+++ ts/main.ts
@@ -0,0 +1,23 @@
+let offsetTs: number = 0;
+const sliderlineTs = document.querySelector('.slider-line') as HTMLElement;
+const nextButton = document.querySelector('.slider-next') as HTMLElement;
+const prevButton = document.querySelector('.slider-prev') as HTMLElement;
+
+nextButton.onclick = e => {
+    e.preventDefault;
+    offsetTs = offsetTs + 256;
+     if (offsetTs > 768) {
+         offsetTs = 0;
+     }
+     sliderlineTs.style.left = -offsetTs + 'px';
+
+}
+
+prevButton.onclick = e => {
+    e.preventDefault;
+    offsetTs = offsetTs - 256;
+    if (offsetTs < 0) {
+        offsetTs = 768;
+    }
+    sliderlineTs.style.left = -offsetTs + 'px';
+}
