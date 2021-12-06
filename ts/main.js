67b86823e2b4e8774ae824f8a454789a3f27080d
reebelbeebel <reebelbeebel@yandex.ru>
2021-12-06 10:48:31 +0300

    Edit main.ts

--- ts/main.js
+++ ts/main.js
@@ -3,16 +3,16 @@ let offsetTs = 0;
 const sliderlineTs = document.querySelector('.slider-line');
 const nextButton = document.querySelector('.slider-next');
 const prevButton = document.querySelector('.slider-prev');
-nextButton.onclick = e => {
-    e.preventDefault;
+nextButton.onclick = (e) => {
+    e.preventDefault();
     offsetTs = offsetTs + 256;
     if (offsetTs > 768) {
         offsetTs = 0;
     }
     sliderlineTs.style.left = -offsetTs + 'px';
 };
-prevButton.onclick = e => {
-    e.preventDefault;
+prevButton.onclick = (e) => {
+    e.preventDefault();
     offsetTs = offsetTs - 256;
     if (offsetTs < 0) {
         offsetTs = 768;
